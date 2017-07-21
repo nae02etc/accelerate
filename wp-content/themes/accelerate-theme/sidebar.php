ea29c431560eaba56d6340cd960fc19aa004be4e
natalie <natalie.mm@gmail.com>
2017-07-20 19:41:53 -0700

    catch up with lessons

--- wp-content/themes/accelerate-theme/sidebar.php
+++ wp-content/themes/accelerate-theme/sidebar.php
@@ -4,29 +4,25 @@
  *
  * @package WordPress
  * @subpackage Accelerate Marketing
- * @since Accelerate Marketing 1.0
+ * @since Accelerate Marketing 2.0
  */
 ?>
 
-<aside class="sidebar">
-	<?php if ( is_active_sidebar( 'sidebar-1' ) ) {?>
-		<?php dynamic_sidebar( 'sidebar-1' ); ?>	
-	<?php }	else { ?>
+<div id="sidebar">
 
-			<div class="post-author">
-					<div class="avatar"><img src="upload/Emily2.jpg" alt=""></div>
-					<div class="intro-text">I’m Emily, the lead developer at <a href="http://skillcrush.com/">Skillcrush</a> as well as an instructor in Ruby and JavaScript. This is my blog full of inspiration.</div>
+			<div class="widget widget_text">
+					<div class="intro-text">This is a practice site for our <a href="http://skillcrush.com/">Skillcrush</a> students. <a href="https://skillcrush.com/wordpress-blueprint-package-first-to-know">We’d love to see you in class!</a></div>
 				</div>
-			
-				<div class="widget widget_search-form">
-					<form role="search" method="get" class="search-form" action="">
-						<input type="text" class="input" placeholder="search" value="" name="s">
-						<input type="button" class="input-btn" value="">
+
+				<div class="widget widget_search">
+					<form role="search" method="get" id="searchform" class="searchform" action="<?php echo esc_url( home_url( '/' ) ); ?>">
+						<input type="text" value="<?php echo get_search_query(); ?>" name="s" id="s" placeholder="search" />
+						<input type="submit" id="searchsubmit" value="" />
 					</form>
 				</div>
 
 				<div class="widget widget_categories">
-					<h3 class="widget-title">Categories</h3>		
+					<h2 class="widget-title">Categories</h2>
 					<ul>
 						<li><a href="">Books </a></li>
 						<li><a href="">My Notes</a></li>
@@ -37,7 +33,7 @@
 				</div>
 
 				<div class="widget widget_archives">
-					<h3 class="widget-title">Archives</h3>		
+					<h2 class="widget-title">Archives</h2>
 					<ul>
 						<li><a href="">September 2014</a></li>
 						<li><a href="">August 2014</a></li>
@@ -48,13 +44,12 @@
 				</div>
 
 				<div class="widget widget_events">
-					<h3 class="widget-title">Events</h3>		
+					<h2 class="widget-title">Events</h3>
 					<ul>
-						<li><a href=""><img src="upload/Event-1.jpg" alt=""></a></li>
-						<li><a href=""><img src="upload/Event-2.jpg" alt=""></a></li>
-						<li><a href=""><img src="upload/Event-3.jpg" alt=""></a></li>
+						<li><a href="#"><img src="<?php echo get_template_directory_uri(); ?>/img/Brooklyn.png" alt=""/></a></li>
+						<li><a href="#"><img src="<?php echo get_template_directory_uri(); ?>/img/Rails.png" alt=""/></a></li>
+						<li><a href="#"><img src="<?php echo get_template_directory_uri(); ?>/img/Etsy.png" alt=""/></a></li>
 					</ul>
 				</div>
-		<?php } ?>
-	
-</aside>
+
+</div>
